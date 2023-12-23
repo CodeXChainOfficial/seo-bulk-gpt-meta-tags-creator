671c91cb829af1532dc39edcd1611df8ce90332c
Andrea Bello <94094948+CodeXChainOfficial@users.noreply.github.com>
2023-12-24 04:40:34 +0700

    Update home.py

--- home.py
+++ home.py
@@ -14,11 +14,7 @@ st.set_page_config(
 
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
-with st.sidebar:    
-    menu = sac.menu([
-        sac.MenuItem('Bulk Meta', icon='google'),
-    ], index=0, format_func='upper', size='middle', indent=30, open_index=None, open_all=True, return_index=False)
-           
+
 if menu == 'Bulk Meta':
     bulk_meta.createPage()
    
