e8191a3879b76d850402d03f65e0760807d5f4ce
Andrea Bello <94094948+CodeXChainOfficial@users.noreply.github.com>
2023-12-24 04:50:37 +0700

    Update bulk_meta.py

--- views/bulk_meta.py
+++ views/bulk_meta.py
@@ -70,7 +70,7 @@ def createPage():
     
     # Inserindo animação na primeira coluna
     with colunhead:
-        st.image(fc.open_image("./assets/robozin2.png"))
+        st.image(fc.open_image(""))
 
     # Inserindo informações de contatos na segunda coluna
     with colundhead2:
