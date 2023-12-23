7c0481bd861ac582b10c5dac8db761b159c7ddd9
Andrea Bello <94094948+CodeXChainOfficial@users.noreply.github.com>
2023-12-24 04:43:12 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,21 +3,20 @@ import streamlit_antd_components as sac
 from views import bulk_meta, about
 from PIL import Image
 
-favicon = Image.open("./assets/favicon.png")
 
 st.set_page_config(
     page_title="Bulk Meta Tags Creator",
-
     layout="wide",
     initial_sidebar_state='collapsed',
 )
-if menu == 'Bulk Meta':
-    bulk_meta.createPage()
 
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
-
+with st.sidebar:    
+    menu = sac.menu([
+        sac.MenuItem('Bulk Meta', icon='google'),
+    ], index=0, format_func='upper', size='middle', indent=30, open_index=None, open_all=True, return_index=False)
+           
 if menu == 'Bulk Meta':
     bulk_meta.createPage()
    
-
